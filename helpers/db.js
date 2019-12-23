fcba540935a9e82a82d4af18c62b4f337416000c
ywalakamar <barxwells@gmail.com>
2019-12-23 16:05:57 +0300

    Refactor db file in helpers folder - [ ] closes #53

--- helpers/db.js
+++ helpers/db.js
@@ -5,100 +5,93 @@ const pool = new Pool({ connectionString: config.DATABASE_URL });
 
 /**Database  */
 class Database {
+  /** Create table function
+   * * @returns {Promise}
+   */
+  create = async (table, statement) => {
+    try {
+      await pool.query(`CREATE TABLE IF NOT EXISTS ${table}(${statement})`);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  /** Drop table function
+   * * @returns {Promise}
+   */
+  drop = async table => {
+    try {
+      await pool.query(`DROP TABLE IF EXISTS ${table}`);
+    } catch (error) {
+      console.log(error);
+    }
+  };
   /** Create users table
    * * @returns {Promise}
    */
   createUsersTable = async () => {
-    try {
-      await pool.query(
-        `CREATE TABLE IF NOT EXISTS users(id SERIAL PRIMARY KEY, email VARCHAR(50) UNIQUE NOT NULL, password VARCHAR(100) NOT NULL, dateCreated TIMESTAMP, dateModified TIMESTAMP)`
-      );
-    } catch (error) {
-      console.log(error);
-    }
+    await this.create(
+      "users",
+      "id SERIAL PRIMARY KEY, email VARCHAR(50) UNIQUE NOT NULL, password VARCHAR(100) NOT NULL, dateCreated TIMESTAMP, dateModified TIMESTAMP"
+    );
   };
 
   /**Drop users table
    ** @returns {Promise}
    */
   dropUsersTable = async () => {
-    try {
-      await pool.query(`DROP TABLE IF EXISTS users`);
-    } catch (error) {
-      console.log(error);
-    }
+    await this.drop("users");
   };
 
   /** Create owners table
    * * @returns {Promise}
    */
   createOwnersTable = async () => {
-    try {
-      await pool.query(
-        `CREATE TABLE IF NOT EXISTS owners(id SERIAL PRIMARY KEY,firstName VARCHAR(50) NOT NULL,lastName VARCHAR(50) NOT NULL, email VARCHAR(50) UNIQUE NOT NULL,createdBy VARCHAR(50) REFERENCES users(email),dateCreated TIMESTAMP,dateModified TIMESTAMP)`
-      );
-    } catch (error) {
-      console.log(error);
-    }
+    await this.create(
+      "owners",
+      "id SERIAL PRIMARY KEY,firstName VARCHAR(50) NOT NULL,lastName VARCHAR(50) NOT NULL, email VARCHAR(50) UNIQUE NOT NULL,createdBy VARCHAR(50) REFERENCES users(email),dateCreated TIMESTAMP,dateModified TIMESTAMP"
+    );
   };
 
   /**
    * * @returns {Promise}
    */
   dropOwnersTable = async () => {
-    try {
-      await pool.query(`DROP TABLE IF EXISTS owners`);
-    } catch (error) {
-      console.log(error);
-    }
+    this.drop("owners");
   };
 
   /** Create owners table
    * * @returns {Promise}
    */
   createCategoriesTable = async () => {
-    try {
-      await pool.query(
-        `CREATE TABLE IF NOT EXISTS categories(id SERIAL PRIMARY KEY,categoryName VARCHAR(50) NOT NULL,createdBy INTEGER REFERENCES users(id),dateCreated TIMESTAMP, dateModified TIMESTAMP)`
-      );
-    } catch (error) {
-      console.log(error);
-    }
+    await this.create(
+      "categories",
+      "id SERIAL PRIMARY KEY,categoryName VARCHAR(50) NOT NULL,createdBy INTEGER REFERENCES users(id),dateCreated TIMESTAMP, dateModified TIMESTAMP"
+    );
   };
 
   /**
    * * @returns {Promise}
    */
   dropCategoriesTable = async () => {
-    try {
-      await pool.query(`DROP TABLE IF EXISTS categories`);
-    } catch (error) {
-      console.log(error);
-    }
+    await this.drop("categories");
   };
 
   /** Create items table
    * * @returns {Promise}
    */
   createItemsTable = async () => {
-    try {
-      await pool.query(
-        `CREATE TABLE IF NOT EXISTS items(id SERIAL PRIMARY KEY,categoryId INTEGER REFERENCES categories(id),ownerId INTEGER REFERENCES owners(id),itemName VARCHAR(50) NOT NULL,createdBy INTEGER REFERENCES users(id),dateCreated TIMESTAMP,dateModified TIMESTAMP)`
-      );
-    } catch (error) {
-      console.log(error);
-    }
+    await this.create(
+      "items",
+      "id SERIAL PRIMARY KEY,categoryId INTEGER REFERENCES categories(id),ownerId INTEGER REFERENCES owners(id),itemName VARCHAR(50) NOT NULL,createdBy INTEGER REFERENCES users(id),dateCreated TIMESTAMP,dateModified TIMESTAMP"
+    );
   };
 
   /**
    * * @returns {Promise}
    */
   dropItemsTable = async () => {
-    try {
-      await pool.query(`DROP TABLE IF EXISTS items`);
-    } catch (error) {
-      console.log(error);
-    }
+    await this.drop("items");
   };
 
   /**
